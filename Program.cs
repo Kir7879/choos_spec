822bf7f3f4e053a7d89b1f7e0df811b53573f684
Пашков Кирилл <[kir12.78@mail.ru]>
2023-06-26 17:03:39 +0300

    First version

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿static void Choos_spec()
+{
+    Console.Write("Введите количество строк, которые хотите ввести: ");
+    int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
+    string[] strs = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+        strs[i] = Console.ReadLine(); //Заполняем его
+    }
+
+    Console.ReadLine();
+}
+
+Choos_spec();
+
+
+
+
