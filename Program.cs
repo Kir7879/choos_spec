3e189021e463624c60b495142ab2dd7e226d6c08
Пашков Кирилл <[kir12.78@mail.ru]>
2023-06-30 09:46:38 +0300

    Замечание

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿static void Choos_spec()
+﻿
+static void Choos_spec()
 {
     Console.Write("Введите количество строк, которые хотите ввести: ");
     int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
@@ -8,21 +9,16 @@
     {
         Console.Write("Введите строку №{0}:\r\n    ", i + 1);
         strs[i] = Console.ReadLine(); //Заполняем его
-        strs[i] = strs[i].Substring(0, 3);
+        //strs[i] = strs[i].Substring(0, 3);
     }
+
     Console.WriteLine("Выводим массив по-строчно из первых трех символов строки:");
     {
         for (int i = 0; i < n; i++)
         {
-            Console.WriteLine(strs[i]);
+            if (strs[i].Length <= 3)
+            {
+                Console.WriteLine(strs[i]);
+            }
         }
-    }
-    Console.WriteLine("Нажмите [Enter] для выхода из программы");
-    Console.ReadLine();
-}
-
-Choos_spec();
-
-
-
-
+        
